dbf64d7ccad90608cf4525c2675a55ebdcd74c5d
sir-abbey <kaybiel4u@gmail.com>
2026-02-08 03:16:19 +0100

    Working on BackendApplication

--- backend/src/main/java/com/abiodunelijah/contact/mappers/ContactMapper.java
+++ backend/src/main/java/com/abiodunelijah/contact/mappers/ContactMapper.java
@@ -1,29 +1,50 @@
 package com.abiodunelijah.contact.mappers;
 
+import com.abiodunelijah.contact.dtos.AddressRequestDto;
 import com.abiodunelijah.contact.dtos.ContactRequestDto;
 import com.abiodunelijah.contact.entities.Address;
 import com.abiodunelijah.contact.entities.Contact;
 
 public class ContactMapper {
 
-    public static ContactRequestDto mapToEntity(Contact contact){
+    public static ContactRequestDto mapToDto(Contact contact) {
+        AddressRequestDto addressRequestDto = null;
+
+        if (contact.getAddress() != null) {
+            addressRequestDto = new AddressRequestDto();
+            addressRequestDto.setStreetNumber(contact.getAddress().getStreetNumber());
+            addressRequestDto.setStreetName(contact.getAddress().getStreetName());
+            addressRequestDto.setPostalCode(contact.getAddress().getPostalCode());
+            addressRequestDto.setState(contact.getAddress().getState());
+            addressRequestDto.setCountry(contact.getAddress().getCountry());
+        }
 
         return ContactRequestDto.builder()
                 .firstName(contact.getFirstName())
                 .lastName(contact.getLastName())
                 .phoneNumber(contact.getPhoneNumber())
-                .addressRequestDto()
+                .addressRequestDto(addressRequestDto)
                 .build();
     }
 
-    public static Contact mapToDto(ContactRequestDto contactRequestDto){
+    public static Contact mapToEntity(ContactRequestDto contactRequestDto) {
+        Address address = null;
+
+        if (contactRequestDto.getAddressRequestDto() != null) {
+            address = Address.builder()
+                    .streetNumber(contactRequestDto.getAddressRequestDto().getStreetNumber())
+                    .streetName(contactRequestDto.getAddressRequestDto().getStreetName())
+                    .postalCode(contactRequestDto.getAddressRequestDto().getPostalCode())
+                    .state(contactRequestDto.getAddressRequestDto().getState())
+                    .country(contactRequestDto.getAddressRequestDto().getCountry())
+                    .build();
+        }
 
         return Contact.builder()
                 .firstName(contactRequestDto.getFirstName())
                 .lastName(contactRequestDto.getLastName())
                 .phoneNumber(contactRequestDto.getPhoneNumber())
-                .address()
+                .address(address)
                 .build();
     }
-
 }
